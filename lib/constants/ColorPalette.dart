2d200e5ef7c1b0968a9ec944155e0f659fb32089
Maryem El Ajleni <ajlanimeryam@gmail.com>
2022-12-02 14:42:00 +0100

    initial commit

---
+++ lib/constants/ColorPalette.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+class ColorPalette{
+static const lightGreen = Color(0xffcdf5db);
+static const lightBlue = Color(0xff8bd1d3);
+static const pink = Color(0xffc0aec4);
+static const lightPurple= Color(0xffaf7bbd);
+static const darkPurple = Color(0xff390038);
+}
---
+++ lib/widgets/Calendar.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:my_journey/constants/ColorPalette.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+class Calendar extends StatefulWidget {
+  const Calendar({Key? key}) : super(key: key);
+
+  @override
+  State<Calendar> createState() => _CalendarState();
+}
+
+class _CalendarState extends State<Calendar> {
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: ColorPalette.darkPurple,
+      child: TableCalendar(
+        headerStyle: const HeaderStyle(
+          formatButtonVisible: false,
+          leftChevronIcon:
+              Icon(Icons.arrow_left, color: Color.fromARGB(255, 224, 189, 234)),
+          rightChevronIcon: Icon(Icons.arrow_right,
+              color: Color.fromARGB(255, 224, 189, 234)),
+          titleTextStyle:
+              TextStyle(color: ColorPalette.pink, fontWeight: FontWeight.bold),
+        ),
+        daysOfWeekStyle: const DaysOfWeekStyle(
+            weekdayStyle: TextStyle(color: ColorPalette.lightGreen)),
+          
+        calendarStyle: const CalendarStyle(
+          defaultTextStyle: TextStyle(
+            color: ColorPalette.pink
+          ),
+          weekendTextStyle:
+              TextStyle(color: Color.fromARGB(255, 228, 176, 239)),
+          todayDecoration: BoxDecoration(
+            color: ColorPalette.pink,
+            shape: BoxShape.circle,
+          ),
+          isTodayHighlighted: true,
+          selectedDecoration: BoxDecoration(
+            color: ColorPalette.lightPurple,
+            shape: BoxShape.circle,
+          ),
+          selectedTextStyle: TextStyle(color: Colors.white),
+        ),
+        
+        firstDay: DateTime.utc(2010, 10, 16),
+        lastDay: DateTime.utc(2030, 3, 14),
+        focusedDay: _focusedDay,
+        calendarFormat: _calendarFormat,
+        
+        selectedDayPredicate: (day) {
+          // Use `selectedDayPredicate` to determine which day is currently selected.
+          // If this returns true, then `day` will be marked as selected.
+
+          // Using `isSameDay` is recommended to disregard
+          // the time-part of compared DateTime objects.
+          return isSameDay(_selectedDay, day);
+        },
+        onDaySelected: (selectedDay, focusedDay) {
+          if (!isSameDay(_selectedDay, selectedDay)) {
+            // Call `setState()` when updating the selected day
+            setState(() {
+              _selectedDay = selectedDay;
+              _focusedDay = focusedDay;
+            });
+          }
+        },
+        onFormatChanged: (format) {
+          if (_calendarFormat != format) {
+            // Call `setState()` when updating calendar format
+            setState(() {
+              _calendarFormat = format;
+            });
+          }
+        },
+        onPageChanged: (focusedDay) {
+          // No need to call `setState()` here
+          _focusedDay = focusedDay;
+        },
+      ),
+    );
+  }
+}
